ebcd1b28185dc630033d41e058436ab002de38a7
short-dog <b7short@gmail.com>
2023-12-21 01:15:33 -0500

    Completed Day 5 Part 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "src/Day2.h"
 #include "src/Day3.h"
 #include "src/Day4.h"
+#include "src/Day5.h"
 #include <chrono>
 #include <iostream>
 
@@ -9,10 +10,11 @@
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
-    Day1::runDay1();
-    Day2::runDay2();
-    Day3::runDay3();
-    Day4::runDay4();
+    //Day1::runDay1();
+    //Day2::runDay2();
+    //Day3::runDay3();
+    //Day4::runDay4();
+    Day5::runDay5();
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
