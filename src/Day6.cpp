ee35b388a7bea96b9fb16e53b514da744f40e5f7
short-dog <b7short@gmail.com>
2023-12-20 23:35:29 -0500

    Added Day 6

---
+++ src/Day6.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ben Short on 12/20/23.
+//
+
+#include "Day6.h"
---
+++ src/Day6.h
@@ -0,0 +1,16 @@
+//
+// Created by Ben Short on 12/20/23.
+//
+
+#ifndef DAY6_H
+#define DAY6_H
+
+
+
+class Day6 {
+
+};
+
+
+
+#endif //DAY6_H
