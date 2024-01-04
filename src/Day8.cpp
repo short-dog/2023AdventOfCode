b40758f25d92800477168f7f7ae7868b4710b9b8
short-dog <b7short@gmail.com>
2024-01-03 23:20:34 -0500

    Added Day 8

---
+++ src/Day8.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ben Short on 1/3/24.
+//
+
+#include "Day8.h"
---
+++ src/Day8.h
@@ -0,0 +1,16 @@
+//
+// Created by Ben Short on 1/3/24.
+//
+
+#ifndef DAY8_H
+#define DAY8_H
+
+
+
+class Day8 {
+
+};
+
+
+
+#endif //DAY8_H
